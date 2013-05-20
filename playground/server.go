0b0c4eb978542043c447209104b643f9f822e702
aurelian <oancea@gmail.com>
2013-05-20 23:11:32 +0200

    hello path.

--- playground/server.go
+++ playground/server.go
@@ -1,8 +1,18 @@
 package main
 
-import "net/http"
+import (
+  "fmt"
+  "html"
+  "net/http"
+)
 
 func main() {
+
+  http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+  })
+
   http.Handle("/", http.FileServer(http.Dir("public")))
+
   http.ListenAndServe(":9090", nil)
 }
